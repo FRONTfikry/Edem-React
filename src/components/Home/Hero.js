44434d4b58323ce408c84a58c10217417c47a51b
FRONTfikry <edvard2027@gmail.com>
2023-05-08 18:11:27 +0300

    fix: change the anchor to a Link in the to catalog button

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import heroImg from '../../assets/pictures/hero-intro.png'
 
 function Hero() {
@@ -10,7 +12,7 @@ function Hero() {
                     <h1 className="hero__title">EDEM</h1>
                     <h2 className="hero__slogan">Лучшие двери для Вас!</h2>
 
-                    <a className="btn--action hero__btn">В каталог</a>
+                    <Link to="/cart" className="btn--action hero__btn">В каталог</Link>
                 </div>
                 <img src={heroImg} alt="" className="hero__img" />
             </div>
